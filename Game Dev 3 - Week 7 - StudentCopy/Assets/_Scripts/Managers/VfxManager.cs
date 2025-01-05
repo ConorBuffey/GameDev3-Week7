e10b48b7d3bd24f5f95ee5070e9a8f4eb2ade76f
ConorBuffey <c.buffey3244@student.leedsbeckett.ac.uk>
2025-01-05 01:24:30 +0000

    Done

---
+++ Game Dev 3 - Week 7 - StudentCopy/Assets/_Scripts/Managers/VfxManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameDevWithMarco.Singleton;
+using System.Collections;
+
+namespace GameDevWithMarco.Managers
+{
+    public class VfxManager : Singleton<VfxManager>
+    {
+        public void HitStop(float stopDuration)
+        {
+            StartCoroutine(HitStopCoroutine(stopDuration));
+        }
+
+
+
+        IEnumerator HitStopCoroutine(float duration)
+        {
+            Time.timeScale = 0;
+            yield return new WaitForSecondsRealtime(duration);
+            Time.timeScale = 1;
+        }
+    }
+}
